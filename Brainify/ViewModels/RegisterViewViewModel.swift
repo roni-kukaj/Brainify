39539acab153f026be7370600b2600de39c82acc
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2024-01-30 00:26:22 +0100

    /feature/created the backend part for the registration

--- Brainify/ViewModels/RegisterViewViewModel.swift
+++ Brainify/ViewModels/RegisterViewViewModel.swift
@@ -1,1 +1,63 @@
 
+import FirebaseFirestore
+import FirebaseAuth
+import Foundation
+
+
+class RegisterViewViewModel: ObservableObject {
+    @Published var name = ""
+    @Published var email = ""
+    @Published var password = ""
+    @Published var confirmPassword = ""
+    @Published var errorMessage = ""
+    
+    init() {
+        
+    }
+    func register() {
+        guard validate() else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
+            guard let userId = result?.user.uid else {
+                return
+            }
+            self?.insertUserRecord(id: userId)
+        }
+    }
+    
+    private func insertUserRecord(id: String) {
+        let newUser = User(id: id, name: name, email: email, joined: Date().timeIntervalSince1970)
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(id)
+            .setData(newUser.asDictionary())
+    }
+    
+    private func validate() -> Bool {
+        errorMessage = ""
+        guard !name.isEmpty,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              !confirmPassword.trimmingCharacters(in: .whitespaces).isEmpty else {
+                errorMessage = "Please fill in all the fields."
+                return false
+            }
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Please enter a valid email."
+            return false
+        }
+        guard password == confirmPassword else {
+            errorMessage = "Passwords do not match!"
+            return false
+        }
+        guard password.count >= 6 else {
+            return false
+        }
+        
+        return true
+    }
+}
