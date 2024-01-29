35a2cc7382d10855a0011bb1d8f8a010b2c810d4
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2024-01-29 21:41:29 +0100

    Initial Commit

---
+++ Brainify/BrainifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainifyApp.swift
+//  Brainify
+//
+//  Created by Roni Kukaj on 29.1.24.
+//
+
+import SwiftUI
+
+@main
+struct BrainifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
