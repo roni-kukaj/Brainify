9856968324247126f70aad2fe38247ca31882cd4
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2024-01-30 03:19:20 +0100

    /feature/created the front end for the login

--- Brainify/Views/LoginView.swift
+++ Brainify/Views/LoginView.swift
@@ -2,8 +2,38 @@
 import SwiftUI
 
 struct LoginView: View {
+    @StateObject var viewModel = RegisterViewViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView(title: "Log In",
+            subtitle: "Continue your journey!",
+            angle: 15,
+            background: Color(hex: "#78C1F3"))
+        }
+
+        Form {
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage)
+                    .foreGroundcolor(.red)
+            }
+
+            TextField("Email Address", text: $viewModel.email)
+                .textFieldStyle(DefaultTextFieldStyle())
+                .autocorrectionDisabled(true)
+            
+            SecureField("Password", text: $viewModel.password)
+                .textFieldStyle(DefaultTextFieldStyle())
+                .autocorrectionDisabled(true)
+
+            CustomButton(title: "Log In", background: .green) {
+                viewModel.login()
+            }
+            .padding()
+        }
+        .offset(y: -50)
+
+        Spacer()
     }
 }
 
