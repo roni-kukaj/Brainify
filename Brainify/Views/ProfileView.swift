9ce05a9ffaab7787dbd29e743131256af2ff2cbd
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2024-01-30 04:01:46 +0100

    /feature/created the logout and profile funcionality

--- Brainify/Views/ProfileView.swift
+++ Brainify/Views/ProfileView.swift
@@ -2,8 +2,37 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                    .padding()
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Name: ")
+                        Text("")
+                    }
+                    .padding()
+                    HStack {
+                        Text("Email: ")
+                        Text("")
+                    }
+                    .padding()
+                }
+                Button("Log Out") {
+                    viewModel.logOut()
+                }
+                .tint(.red)
+            }
+            .navigationTitle("Profile")
+        }
     }
 }
 
